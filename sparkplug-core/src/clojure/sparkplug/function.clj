e63a3f85173f0ebe2a5170996e35ae939b60e58e
Greg Look <greg@amperity.com>
2019-09-15 10:20:42 -0700

    Remove anonymous function macro approach.

--- sparkplug-core/src/clojure/sparkplug/function.clj
+++ sparkplug-core/src/clojure/sparkplug/function.clj
@@ -30,78 +30,9 @@
 (gen-function Fn1 fn1)
 (gen-function Fn2 fn2)
 (gen-function Fn3 fn3)
-(gen-function VoidFn void-fn)
 (gen-function ComparatorFn comparator-fn)
 (gen-function FlatMapFn1 flat-map-fn)
 (gen-function FlatMapFn2 flat-map-fn2)
 (gen-function PairFlatMapFn pair-flat-map-fn)
 (gen-function PairFn pair-fn)
-
-
-
-;; ## Inline Functions
-
-;; TODO: decide if these are useful
-
-#_
-(defn- anonymous-function-name
-  "Generate a deterministic name for an anonymous function by referencing the
-  ns and source code."
-  [form-ns line column]
-  (->
-    (str form-ns "_anon_" line \_ column)
-    (clojure.lang.Compiler/munge)
-    (symbol)))
-
-
-#_
-(defn- ns-requires
-  "Discover all namespaces required to run code from `form-ns` by examining
-  the `ns-refers` and `ns-aliases`. Returns a set of required namespaces as
-  symbols.
-
-  **NOTE:** This won't catch fully-qualified references to other namespaces!"
-  [form-ns]
-  ;; TODO: are there more namespaces that can be ignored here?
-  (let [implicit? #{"clojure.core"
-                    "clojure.java.io"}]
-    (set/union
-      ;; Namespaces referenced by a local alias.
-      (into #{}
-            (comp
-              (map val)
-              (map str)
-              (remove implicit?)
-              (map symbol))
-            (ns-aliases form-ns))
-      ;; Namespaces referenced by a referred function.
-      (into #{}
-            (comp
-              (map val)
-              (map symbol)
-              (map namespace)
-              (remove implicit?)
-              (map symbol))
-            (ns-refers form-ns)))))
-
-
-#_
-(defmacro fn
-  "Define a function as in `clojure.core/fn` but ensures the result is
-  serializable for use with Spark. Only accepts a single arity signature.
-
-      (fn name? [params*] exprs*)
-
-  If a name is not provided, one will be deterministically generated."
-  [& signature]
-  (let [form-meta (meta &form)
-        line (:line form-meta)
-        column (:column form-meta)
-        [fn-name args & body] (if (symbol? (first signature))
-                                signature
-                                (cons (anonymous-function-name *ns* line column)
-                                      signature))
-        namespaces (ns-requires *ns*)]
-    `(vary-meta
-       (clojure.core/fn ~fn-name ~args ~@body)
-       assoc :requires namespaces)))
+(gen-function VoidFn void-fn)

33d8068ff1c89452d77cabca2cc02fd18d1eedd5
Greg Look <greg@amperity.com>
2019-09-15 10:21:23 -0700

    Update repl user ns.

--- sparkplug-core/dev/user.clj
+++ sparkplug-core/dev/user.clj
@@ -4,12 +4,7 @@
     [clojure.repl :refer :all]
     [clojure.stacktrace :refer [print-cause-trace]]
     [clojure.string :as str]
-    [clojure.tools.namespace.repl :refer [refresh]])
-  (:import
-    sparkplug.function.SerializableFn))
-
-
-(defn find-var-namespaces
-  "Trace the given value to find referenced namespaces."
-  [f]
-  (set (SerializableFn/findVarNamespaces f)))
+    [clojure.tools.namespace.repl :refer [refresh]]
+    [sparkplug.core :as spark]
+    [sparkplug.function :as f]
+    [sparkplug.rdd :as rdd]))

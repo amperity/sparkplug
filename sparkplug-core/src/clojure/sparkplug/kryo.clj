c79cd93677b698c104767273a186d0f9a7e10145
Greg Look <greg@amperity.com>
2019-09-17 19:38:10 -0700

    Test and fix classpath registry searching.

--- sparkplug-core/src/clojure/sparkplug/kryo.clj
+++ sparkplug-core/src/clojure/sparkplug/kryo.clj
@@ -29,7 +29,8 @@
   (:require
     [clojure.java.classpath :as classpath]
     [clojure.java.io :as io]
-    [clojure.string :as str])
+    [clojure.string :as str]
+    [clojure.tools.logging :as log])
   (:import
     (com.esotericsoftware.kryo
       Kryo
@@ -41,71 +42,90 @@
       JarFile)))
 
 
+(def ^:const registry-prefix
+  "SparkPlug registry files must be available under this directory path."
+  "sparkplug/kryo/registry")
+
+
+(def ^:const registry-extension
+  "SparkPlug registry file extension."
+  ".conf")
+
+
+
 ;; ## Registry Search
+
+(defn- registry-path?
+  "True if the given path is a valid registry file name."
+  [path]
+  (and (str/starts-with? path registry-prefix)
+       (str/ends-with? path registry-extension)))
+
 
 (defn- relative-suffix
   "Return the suffix in `b` if it is prefixed by `a`."
   [a b]
-  (let [a (str a)
+  (let [a (str a "/")
         b (str b)]
     (when (str/starts-with? b a)
       (subs b (count a)))))
 
 
 (defn- read-dir-file
-  "Read a file from the given directory."
+  "Read a file from the given directory. Returns a map of registry data."
   [^File dir path]
-  {:path (str dir)
-   :name path
-   :content (slurp (io/file dir path))})
+  (let [file (io/file dir path)]
+    (log/debug "Reading registry configuration from file" (str file))
+    {:path (str dir)
+     :name path
+     :text (slurp file)}))
 
 
 (defn- find-dir-files
-  "Find all files in the given directory matching the prefix."
-  [prefix ^File dir]
+  "Find all files in the given directory matching the registry prefix."
+  [^File dir]
   (->> (file-seq dir)
-       (keep (partial relative-suffix (str dir "/")))
-       (filter #(and (str/starts-with? % prefix)
-                     (str/ends-with? % ".conf")))
+       (keep (partial relative-suffix dir))
+       (filter registry-path?)
        (sort)
        (map (partial read-dir-file dir))))
 
 
 (defn- read-jar-entry
-  "Read an entry in the given jar. Returns a map with `:jar`, `:path`, and
-  `:content` if the entry is in the jar."
+  "Read an entry in the given jar. Returns a map of registry data if the entry
+  is in the jar."
   [^JarFile jar entry-name]
   (when-let [entry (.getEntry jar entry-name)]
+    (log/debugf "Reading registry configuration from JAR entry %s!%s"
+               (.getName jar) entry-name)
     {:path (.getName jar)
      :name entry-name
-     :content (slurp (.getInputStream jar entry))}))
+     :text (slurp (.getInputStream jar entry))}))
 
 
 (defn- find-jar-entries
-  "Find all entries in the given JAR file matching the prefix."
-  [prefix ^JarFile jar]
+  "Find all entries in the given JAR file matching the registry prefix."
+  [^JarFile jar]
   (->> (classpath/filenames-in-jar jar)
-       (filter #(and (str/starts-with? % prefix)
-                     (str/ends-with? % ".conf")))
+       (filter registry-path?)
        (sort)
        (keep (partial read-jar-entry jar))))
 
 
 (defn- find-classpath-files
-  "Find all files on the classpath with the given prefix."
-  [prefix]
-  (concat (mapcat (partial find-dir-files prefix)
-                  (classpath/classpath-directories))
-          (mapcat (partial find-jar-entries prefix)
-                  (classpath/classpath-jarfiles))))
+  "Find all config files on the classpath within the registry prefix."
+  []
+  (concat (mapcat find-dir-files (classpath/classpath-directories))
+          (mapcat find-jar-entries (classpath/classpath-jarfiles))))
 
 
 (defn- classpath-registries
-  "Return a sequence of registry file maps from the classpath. Returns a
-  single entry per distinct config name."
+  "Return a sequence of registry file maps from the classpath. Returns a sorted
+  sequence with a single entry per distinct config name. Files earlier on the
+  classpath will take precedence."
   []
   (->>
-    (find-classpath-files "sparkplug/kryo/registry/")
+    (find-classpath-files)
     (map (juxt :name identity))
     (reverse)
     (into (sorted-map))

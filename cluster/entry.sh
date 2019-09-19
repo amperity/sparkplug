16b420974eeeb4183967ff4f8865b26af49d5afc
Greg Look <greg@amperity.com>
2019-09-19 14:55:47 -0700

    More docker cluster updates to work with Hadoop 3.2.0 and Scala 2.12.

--- cluster/entry.sh
+++ cluster/entry.sh
@@ -6,6 +6,7 @@ shift || exit 2
 
 if [[ -x $SPARK_SCRIPT ]]; then
     export SPARK_NO_DAEMONIZE=1
+    export SPARK_DIST_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath)
     exec "$SPARK_SCRIPT" "$@"
 else
     exec "$COMMAND" "$@"

f537247229931985d32eebb1c7af73f6650b4573
Brandon Vincent <brandon@amperity.com>
2019-10-09 14:36:39 -0700

    Require the Clojure namespace when reading a SerializableFn

--- sparkplug-core/src/java/sparkplug/function/SerializableFn.java
+++ sparkplug-core/src/java/sparkplug/function/SerializableFn.java
@@ -1,8 +1,14 @@
 package sparkplug.function;
 
 
+import clojure.lang.Compiler;
 import clojure.lang.IFn;
+import clojure.lang.RT;
+import clojure.lang.Symbol;
 
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 
@@ -18,6 +24,24 @@ public abstract class SerializableFn implements Serializable {
      * Default empty constructor.
      */
     private SerializableFn() {
+    }
+
+
+    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+        // Attempt to derive the needed Clojure namespace from the function's class name.
+        String namespace = Compiler.demunge(this.f.getClass().getName()).split("/")[0];
+        out.writeObject(namespace);
+        out.writeObject(this.f);
+    }
+
+
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+        String namespace = (String)in.readObject();
+        Symbol namespaceSym = Symbol.intern(namespace);
+        synchronized (RT.REQUIRE_LOCK) {
+            RT.var("clojure.core", "require").invoke(namespaceSym);
+        }
+        this.f = (IFn)in.readObject();
     }
 
 

26e03ec0dc4fe3cb8bc7bbeaba10f6c8097a52f2
Brandon Vincent <brandon@amperity.com>
2020-02-08 18:07:14 -0800

    Prepare 0.1.5-SNAPSHOT

--- sparkplug-sql/project.clj
+++ sparkplug-sql/project.clj
@@ -1,4 +1,4 @@
-(defproject amperity/sparkplug-sql "0.1.4"
+(defproject amperity/sparkplug-sql "0.1.5-SNAPSHOT"
   :description "Clojure API for Apache Spark SQL"
   :url "https://github.com/amperity/sparkplug"
   :scm {:dir ".."}
@@ -9,7 +9,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.10.1"]
-   [amperity/sparkplug-core "0.1.4"]]
+   [amperity/sparkplug-core "0.1.5-SNAPSHOT"]]
 
   :profiles
   {:default

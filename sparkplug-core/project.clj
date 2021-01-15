5dc8a4f6622ab0df73f80d7b7b235d69c4f14990
Brandon Vincent <brandon@amperity.com>
2021-01-14 20:31:09 -0800

    Add experimental Spark 3 profile

--- sparkplug-core/project.clj
+++ sparkplug-core/project.clj
@@ -10,14 +10,7 @@
   :dependencies
   [[org.clojure/clojure "1.10.1"]
    [org.clojure/java.classpath "1.0.0"]
-   [org.clojure/tools.logging "1.0.0"]
-
-   ;; Version conflicts
-   [commons-codec "1.10"]
-   [com.fasterxml.jackson.core/jackson-core "2.7.9"]
-   [com.google.code.findbugs/jsr305 "3.0.2"]
-   [org.scala-lang/scala-reflect "2.12.8"]
-   [org.slf4j/slf4j-api "1.7.25"]]
+   [org.clojure/tools.logging "1.0.0"]]
 
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
@@ -39,4 +32,16 @@
    :spark-2.4
    ^{:pom-scope :provided}
    {:dependencies
-    [[org.apache.spark/spark-core_2.12 "2.4.4"]]}})
+    [[org.apache.spark/spark-core_2.12 "2.4.4"]
+
+     ;; Version conflicts
+     [commons-codec "1.10"]
+     [com.fasterxml.jackson.core/jackson-core "2.7.9"]
+     [com.google.code.findbugs/jsr305 "3.0.2"]
+     [org.scala-lang/scala-reflect "2.12.8"]
+     [org.slf4j/slf4j-api "1.7.25"]]}
+
+   :spark-3.0
+   ^{:pom-scope :provided}
+   {:dependencies
+    [[org.apache.spark/spark-core_2.12 "3.0.1"]]}})
